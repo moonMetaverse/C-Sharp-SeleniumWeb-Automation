2a5302996c4169a37921a9125e4fb2166ddccf9e
moonMetaverse <124890951+moonMetaverse@users.noreply.github.com>
2023-08-01 16:17:35 +0530

    ok

---
+++ SeleniumCS/SeleniumCS/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("https://www.youtube.com");
+driver.Manage().Window.Maximize();
+IWebElement searchText = driver.FindElement(By.Name("search_query"));
+searchText.SendKeys("Bert");
+IWebElement btnSearch = driver.FindElement(By.Id("search-icon-legacy"));
+btnSearch.Click();
+Thread.Sleep(5000);
+driver.Close();
